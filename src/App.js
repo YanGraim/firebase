58a28949b7f8bf920df7650f2d280c462fb7efc2
YanGraim <yangraim@gmail.com>
2024-12-30 14:51:13 -0300

    cria campos para salvar infos no db

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { db } from "./firebaseConnection";
+
 function App() {
+
+  function handleAdd() {
+    alert("Teste")
+  }
+
   return (
     <div>
       <h1>React Js + Firebase</h1>
+
+      <div className="container">
+        <label>Titulo: </label>
+        <textarea type="text" placeholder="Digite o titulo"/>
+
+
+        <label>Autor:</label>
+        <input type="text" placeholder="Autor do post" />
+
+        <button onClick={handleAdd}>Cadastrar</button>
+      </div>
     </div>
   );
 }
