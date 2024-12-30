49ac4a485d4f016f0744b1ce2f645eb1ab24499a
YanGraim <yangraim@gmail.com>
2024-12-30 14:32:52 -0300

    configura firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-
-
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
+    <div>
+      <h1>React Js + Firebase</h1>
     </div>
   );
 }
---
+++ src/firebaseConnection.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAxtCLPWarE662dtKcwRFpo1OJYs1lhyfM",
+    authDomain: "learnfirebase-cde8b.firebaseapp.com",
+    projectId: "learnfirebase-cde8b",
+    storageBucket: "learnfirebase-cde8b.firebasestorage.app",
+    messagingSenderId: "262916389588",
+    appId: "1:262916389588:web:0ac18e5dd244ad9eba5941",
+    measurementId: "G-P8D764MV6N"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+
+  export { db };
