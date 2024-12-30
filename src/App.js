5b7da4934a958330d3276f80876bb08bd4a9b037
YanGraim <yangraim@gmail.com>
2024-12-30 15:08:31 -0300

    useState para pegar o que o usuario digitou

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { db } from "./firebaseConnection";
 import './app.css'
+import { useState } from "react";
 
 function App() {
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
 
   function handleAdd() {
     alert("Teste")
@@ -9,15 +12,14 @@ function App() {
 
   return (
     <div>
-
       <div className="container">
         <h1>React Js + Firebase</h1>
         <label>Titulo: </label>
-        <textarea type="text" placeholder="Digite o titulo"/>
+        <textarea type="text" placeholder="Digite o titulo" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
 
 
         <label>Autor:</label>
-        <input type="text" placeholder="Autor do post" />
+        <input type="text" placeholder="Autor do post" value={autor} onChange={(e) => setAutor(e.target.value)}/>
 
         <button onClick={handleAdd}>Cadastrar</button>
       </div>
